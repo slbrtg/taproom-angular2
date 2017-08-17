505adabce29ebe125463709f7f1dc24847fcbf19
Saul Artiga <slbrtg@gmail.com>
2017-08-16 20:02:46 -0700

    present kombucha kegs that are available on tap via an array and one component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,14 +4,34 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <h1 class="jumbotron">Welcome to The Booch</h1>
+  <h1>For Patrons,</h1>
+  <h2>Click a booch to tap a pint bruh bruh</h2>
+  <br>
+  <div id = "patron-kegs" [class]="status(chosenKeg)" (click)="tap(chosenKeg)" *ngFor="let chosenKeg of kegs">
+    <h2>{{chosenKeg.name}}</h2>
+    <h3>Flavor: {{chosenKeg.flavor}}</h3>
+    <h4>Made By: {{chosenKeg.brand}}</h4>
+    <h4>Price: {{chosenKeg.price}}$</h4>
+  </div>
   `
 })
-
+//[class]="status(chosenKeg)" (click)="tap(chosenKeg)"
 export class AppComponent {
+  selectedKeg = null;
+
   // constructor(public name: string, public brand: string, public price: number, public flavor: string){}
-  masterKegList: Keg[] = [
+  kegs: Keg[] = [
     new Keg("Bucha", "Kom", 4, "Lavender Honey"),
-    new Keg("Synergy", "Natural Something", 8, "Grass")
+    new Keg("Synergy", "Natural Something", 8, "Grass"),
+    new Keg("The Booch", "Booch Daddy", 1, "Roquefort")
   ]
+
+  status(chosenKeg){
+    //do things here
+  }
+
+  tap(chosenKeg){
+    //do stuff
+  }
 }
---
+++ app/keg-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'keg-list',
+  template: `
+
+  `
+})
+
+export class KegListComponent{}
